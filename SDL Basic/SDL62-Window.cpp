6b7cddd18da9ed88ff86bfaa65d2199fc0c949db
arfneto <feitosaneto@terra.com.br>
2019-08-08 02:05:35 -0300

    enter renderer and texture for lesson 63

--- SDL Basic/SDL62-Window.cpp
+++ SDL Basic/SDL62-Window.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 
 
-int main61(int argc, char** argv)
+int main62(int argc, char** argv)
 {
 	SDL_version SDL_compile_version;
 	SDL_version SDL_dll_version;
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
 	SDL_GetVersion(&SDL_dll_version);
 	printf("SDL runtime %d.%d.%d\n",
 		SDL_dll_version.major,
@@ -24,12 +27,43 @@ int main61(int argc, char** argv)
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		cout << "SDL Error: " << SDL_GetError() << endl;
-		return EXIT_FAILURE;
+		return 1;
 	}
 	else
 	{
 		cout << "SDL Ready!" << SDL_GetError() << endl;
 	}
+
+	SDL_Window* window = SDL_CreateWindow(
+		"Particle Fire Explosion",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN
+	);
+
+	if (window == NULL)
+	{
+		SDL_Quit();
+		return 2;
+	}
+
+	SDL_Event event;
+	bool quit = false;
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				quit = true;
+			}	// end if
+		}	// end while
+	}	// end while
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return EXIT_SUCCESS;
 }	// end main()
