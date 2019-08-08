57feb8d627fcfa847f3d11ee528190d2a2beae18
arfneto <feitosaneto@terra.com.br>
2019-08-07 21:01:03 -0300

    Add project files.

---
+++ SDL Basic/SDL Basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello World!\n";
+	return 0;
+}	// end main()
